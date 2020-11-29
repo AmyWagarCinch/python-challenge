ff7862ebb8fa27265247945c6b185c76b8da145a
AmyWagarCinch <gretylschmetyl@gmail.com>
2020-11-28 21:44:53 -0500

    creating candidate list

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -14,21 +14,30 @@ with open(csvpath) as csvfile:
         print(row)
         break
 
+# create variables to store data
 total_votes = []
-total_profit = []
-monthly_profit_change = []
+candidates = []
+voter_id = []
+candidate = []
+candidate_names = []
+percentage = []
+vote_count = []
+winner = 0
+winner_candidate = ""
 
+# Read the csv file
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     header = next(csvreader)
     for row in csvreader:
         # Append the total votes and total ____ to their corresponding lists
         total_votes.append(row[0])
-        #total_profit.append(int(row[1]))
-    # Iterate through the profits in order to get the monthly change in profits
-    #for i in range(len(total_profit)-1):
-        #Take the difference between two months and append to monthly profit change
-        #monthly_profit_change.append(total_profit[i+1]-total_profit[i])
+        candidate.append(row[2])
+        
+    # Create a list to keep candidate names
+    for row in candidate: 
+        if row not in candidate_names:
+            candidate_names.append(row)
+            
+
 print(f"Total Votes Cast: {len(total_votes)}")
-#print(f"Total: ${sum(total_profit)}")
-#print(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
